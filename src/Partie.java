7ef608f184cbe5c0a88a84405b5e91c7dc304740
sebastienjean <baz.jean@gmail.com>
2013-09-29 11:54:21 +0200

    Reviewed code, added some TODOs
    removed useless (because unused) classes.

--- src/Partie.java
+++ src/Partie.java
@@ -1,33 +1,43 @@
+// TODO (fix) declare model-related classes in a package
+// e.g. fr.iutvalence.java.mp.potavscapy
+
+
+// TODO (fix) write a comment that really describe the intent of this class
+// according to the model
 /**  
  * class Partie declaration : the principal class
  */ 
+// TODO (fix) rename class (english)
 public class Partie
 {
-    
+    // TODO (fix) simply comment, it is useless to say that it is a field declaration
     /** 
      * game begining declaration : notice the game start 
      */
     public boolean begining;
     
-    
+    // TODO (fix) simply comment, it is useless to say that it is a field declaration
     /**
      * game ending declaration : notice the game finish
      */
     public boolean ending;
     
     
+    // TODO (fix) rename field
+    // TODO (fix) your model must be player-independant 
     /**
      * notice if the player is against the IA (0) or against an other player (1)
      */
     public boolean multijoueur;
     
-    
+    // TODO (fix) rename field
+    // TODO (fix) your model must be player-independant     
     /**
      * notice if the player choose the capybara (0) or the potamochère (1)
      */
     public boolean potamochereChoisi;
     
-    
+    // TODO (fix) clarify comment and complete it
     /**
      * Install the base : How many players (nbj) and which character is chosen (perso)
      * @param nbj 
@@ -38,8 +48,6 @@ public class Partie
         this.multijoueur = nbj;
         this.potamochereChoisi = perso;
         this.begining = true;
-        this.ending = false;
-       
+        this.ending = false; 
     } 
-    
 }
