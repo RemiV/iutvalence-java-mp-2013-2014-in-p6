6130b80190555b914c98d902967109dd8c8ae38b
RemiV <vivierem@laposte.net>
2013-10-07 12:13:29 +0200

    Class MapException created

---
+++ src/fr/iutvalence/java/mp/potavscapy/MapException.java
@@ -0,0 +1,10 @@
+package fr.iutvalence.java.mp.potavscapy;
+
+/**
+ * This class defines the possible sizes of the map
+ * @author vivierr
+ *
+ */
+public class MapException extends Exception
+{
+}
