e240490df35c7efb83a820ce6ebd793aeb3f18fa
RemiV <vivierem@laposte.net>
2013-10-14 11:47:16 +0200

    class Player created.

---
+++ src/fr/iutvalence/java/mp/potavscapy/Player.java
@@ -0,0 +1,23 @@
+package fr.iutvalence.java.mp.potavscapy;
+
+/**
+ * Define the type of player and what he must do.
+ * @author vivierr
+ *
+ */
+public class Player
+{
+    
+    /**
+     * Define the square where the player want to go.
+     */
+    public Location whereGo;
+    
+    /**
+     * Define the square where the player want to use his Man or his Boa. 
+     */
+    public Location WhereBonus;
+    
+    
+    
+}
