83d5067ebab66c98509c00b6ee54f6cad1bad0c7
RemiV <vivierem@laposte.net>
2013-11-04 11:10:20 +0100

    TODO fixed

--- src/fr/iutvalence/java/mp/potavscapy/Main.java
+++ src/fr/iutvalence/java/mp/potavscapy/Main.java
@@ -1,9 +1,14 @@
 package fr.iutvalence.java.mp.potavscapy;
 
-//TODO (fix) write comment
+/**
+ * Create a main class for set the game.
+ * @author vivierr
+ *
+ */
 public class Main
 {
-    // TODO (fix) write comment
+    // TODO (fixed) write comment
+    
     public static void main(String[] args)
     {
         new Game(nbj, perso, sizemap).play();
