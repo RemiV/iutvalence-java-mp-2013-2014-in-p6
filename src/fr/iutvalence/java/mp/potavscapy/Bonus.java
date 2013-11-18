9f175d728107c45a6aa63f6ac0b171d70c3624de
RemiV <vivierem@laposte.net>
2013-11-18 12:30:32 +0100

    Class Bonus created.

---
+++ src/fr/iutvalence/java/mp/potavscapy/Bonus.java
@@ -0,0 +1,55 @@
+package fr.iutvalence.java.mp.potavscapy;
+
+/**
+ *
+ * 
+ * @author vivierr
+ *
+ */
+public class Bonus
+{
+    
+    /**
+     * Define the APPLE bonus : the potamochère need it to win.
+     */
+    public final static int APPLE=0;
+    
+
+    /**
+     * Define the PUMPKIN bonus : it worths 10 APPLES.
+     */
+    public final static int PUMPKIN=1;
+
+    /**
+     * Define the ALLIGATOR bonus : the character who found it skip a turn.
+     */
+    public final static int ALLIGATOR=2;
+
+    /**
+     * Define the MAKI CATTA bonus : the character who found it win a turn.
+     */
+    public final static int MAKICATTA=3;
+
+    /**
+     * Define the ARA bonus : it prevents the next animal who will be found.
+     */
+    public final static int ARA=4;
+
+    /**
+     * Define the GARARUFA bonus : changes the square in a WATER square.
+     */
+    public final static int GARARUFA = 5;
+
+    /**
+     * Define the MAN bonus : it cuts a TREE (the square become a SWAMP).
+     */
+    public final static int MAN = 6;
+
+    /**
+     * Define the BEE bonus : it creates a TREE (a SWAMP become a TREE).
+     */
+    public final static int BEE = 7;
+    
+    
+    
+}
